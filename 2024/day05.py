b7e0470a7a6df97dd9f63bb7c7585b9e7794a3cc
Ivett Ördög <devillmeister@gmail.com>
2024-12-04 08:58:30 +0100

    Scaffold

---
+++ 2024/day05.py
@@ -0,0 +1,11 @@
+import os
+from utils import data_files_for
+
+if __name__ == "__main__":
+    for file in data_files_for(os.path.basename(__file__)):
+        for line in file:
+            print(line)
+
+        print("\n--- Part one ---")
+
+        print("\n--- Part two ---")
