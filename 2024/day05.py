9537f8b1651bbb9a1826b6c052c00c77d1975570
Ivett Ördög <devillmeister@gmail.com>
2024-12-05 10:16:41 +0100

    Day05: Part 1 done

--- 2024/day05.py
+++ 2024/day05.py
@@ -2,38 +2,62 @@ import os
 import re
 from utils import data_files_for
 
-def is_correctly_ordered(update, follows):
+class Rules:
+    def __init__(self):
+        self.precedes = {}
+        self.follows = {}
+
+    def add_rule(self, a, b):
+        if b not in self.precedes:
+            self.precedes[b] = []
+        if a not in self.follows:
+            self.follows[a] = []
+        self.precedes[b].append(a)
+        self.follows[a].append(b)
+
+    def cant_follow(self, page):
+        return self.precedes.get(page, [])
+
+    def cant_precede(self, page):
+        return self.follows.get(page, [])
+
+def is_correctly_ordered(update, rules):
     banned = set()
+    banned_by = {}
     for page in update:
+
         if page in banned:
             return False
-        if str(page) in follows:
-            banned.add(follows[str(page)])
+        banned.update(rules.cant_follow(page))
+        for banned_page in rules.cant_follow(page):
+            if banned_page not in banned_by:
+                banned_by[banned_page] = []
+            banned_by[banned_page].append(page)
     return True
 
 if __name__ == "__main__":
+
+    rulePattern = re.compile(r'(\d+)\|(\d+)')
+    listPattern = re.compile(r'(\d+,)+\d+')
+
     for file in data_files_for(os.path.basename(__file__)):
-        precedes = {}
-        follows = {}
+        rules = Rules()
         updates = []
-        rulePattern = re.compile(r'(\d+)\|(\d+)')
-        listPattern = re.compile(r'(\d+,)+\d+')
+
         for line in file:
             ruleMatch = rulePattern.match(line)
             if ruleMatch:
-                precedes[ruleMatch.group(1)] = ruleMatch.group(2)
-                follows[ruleMatch.group(2)] = ruleMatch.group(1)
+                rules.add_rule(int(ruleMatch.group(1)), int(ruleMatch.group(2)))
             elif listPattern.match(line):
                 updates.append([int(num) for num in line.split(',')])
 
         print("\n--- Part one ---")
 
-
-        correct = [update for update in updates if is_correctly_ordered(update, follows)]
+        correct = [update for update in updates if is_correctly_ordered(update, rules)]
+        midNumber = [c[(len(c) - 1) // 2] for c in correct]
 
         print(f"Correctly ordered updates: {len(correct)}")
-        print(correct)
+        print(f"Solution: {sum(midNumber)}")
 
         print("\n--- Part two ---")
 
-        exit(0)
