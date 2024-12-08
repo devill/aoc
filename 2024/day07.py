b0b27ce8ced00eff87f798755d4c1732b96a7420
Ivett Ördög <devillmeister@gmail.com>
2024-12-08 09:14:45 +0100

    Day07: Part 02 done

--- 2024/day07.py
+++ 2024/day07.py
@@ -5,9 +5,9 @@ def parse_line(line):
     result_part, operands_part = line.split(':')
     result_value = int(result_part.strip())
     operands = list(map(int, operands_part.strip().split()))
-    return {"result": result_value, "operands": operands}
+    return (result_value, operands)
 
-def hasSolution(result, operands):
+def hasSolution(result, operands, use_concentration):
     if result < 0:
         return False
     if len(operands) == 0:
@@ -17,10 +17,21 @@ def hasSolution(result, operands):
     remaining_operands = operands[:-1]
 
     if result % last_operand == 0:
-        if hasSolution(result // last_operand, remaining_operands):
+        if hasSolution(result // last_operand, remaining_operands, use_concentration):
             return True
 
-    return hasSolution(result - last_operand, remaining_operands)
+    if hasSolution(result - last_operand, remaining_operands, use_concentration):
+        return True
+
+    if use_concentration:
+        digits = len(str(last_operand))
+        if str(result)[-digits:] == str(last_operand):
+            newResult = int(str(result)[:-digits])
+            if hasSolution(newResult, remaining_operands, use_concentration):
+                return True
+
+    return False
+
 
 
 if __name__ == "__main__":
@@ -30,11 +41,20 @@ if __name__ == "__main__":
 
         print("\n--- Part one ---")
 
-        solvable = [line['result'] for line in data if hasSolution(line["result"], line["operands"])]
+        solvable = [
+            result for result, operands in data if hasSolution(result, operands, False)
+        ]
 
         print(f"Number of solvable equations: {len(solvable)}")
         print(f"Sum of solutions: {sum(solvable)}")
 
         print("\n--- Part two ---")
+
+        solvable = [
+            result for result, operands in data if hasSolution(result, operands, True)
+        ]
+
+        print(f"Number of solvable equations: {len(solvable)}")
+        print(f"Sum of solutions: {sum(solvable)}")
 
         # exit(0)
