72e6449b44a31c6c1ec3e25d47807992c20336ab
Ivett Ördög <devillmeister@gmail.com>
2024-12-10 11:18:32 +0100

    Day10: Refactored

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,81 +1,60 @@
 import os
 from utils import data_files_for
 
-def print_touched(data, touched):
+def neighbors(location, data):
+    i, j = location
+    if i - 1 >= 0:
+        yield (i - 1, j)
+    if i + 1 < len(data):
+        yield (i + 1, j)
+    if j - 1 >= 0:
+        yield (i, j - 1)
+    if j + 1 < len(data[0]):
+        yield (i, j + 1)
+
+def build_trail_graph(data):
+    graph = {}
+    trailheads = []
+    peaks = []
     for i, line in enumerate(data):
-        for j, num in enumerate(line):
-            if (i, j) in touched:
-                print(num, end="")
-            else:
-                print(".", end="")
-        print()
+        for j, elevation in enumerate(line):
+            graph[(i, j)] = []
+            if elevation == 0:
+                trailheads.append((i, j))
+            if elevation == 9:
+                peaks.append((i, j))
+            if elevation < 9:
+                for ni, nj in neighbors((i, j), data):
+                    if data[ni][nj] == elevation + 1:
+                        graph[(i, j)].append((ni, nj))
+    return graph, trailheads, peaks
 
+def find_reachable_peaks_with_multiplicity(data):
+    graph, trailheads, peaks = build_trail_graph(data)
+    return [search_peaks(trailhead, graph, peaks) for trailhead in trailheads]
 
-def calculate_head_scores(trailheads):
-    current_steps = [{trailhead} for trailhead in trailheads]
-    next_steps = [set() for _ in range(len(trailheads))]
-    next_elevation = 1
-    while next_elevation < 10:
-        for i, trailhead_steps in enumerate(current_steps):
-            for current_step in trailhead_steps:
-                if current_step[0] - 1 >= 0 and data[current_step[0] - 1][current_step[1]] == next_elevation:
-                    next_steps[i].add((current_step[0] - 1, current_step[1]))
-                if current_step[0] + 1 < len(data) and data[current_step[0] + 1][current_step[1]] == next_elevation:
-                    next_steps[i].add((current_step[0] + 1, current_step[1]))
-                if current_step[1] - 1 >= 0 and data[current_step[0]][current_step[1] - 1] == next_elevation:
-                    next_steps[i].add((current_step[0], current_step[1] - 1))
-                if current_step[1] + 1 < len(data[0]) and data[current_step[0]][current_step[1] + 1] == next_elevation:
-                    next_steps[i].add((current_step[0], current_step[1] + 1))
-
-        current_steps = next_steps
-        next_steps = [set() for _ in range(len(trailheads))]
-        next_elevation += 1
-    head_scores = [len(trailhead) for trailhead in current_steps]
-    return head_scores
-
-
-def calculate_head_ratings(trailheads):
-    current_steps = [[trailhead] for trailhead in trailheads]
-    next_steps = [[] for _ in range(len(trailheads))]
-    next_elevation = 1
-    while next_elevation < 10:
-        for i, trailhead_steps in enumerate(current_steps):
-            for current_step in trailhead_steps:
-                if current_step[0] - 1 >= 0 and data[current_step[0] - 1][current_step[1]] == next_elevation:
-                    next_steps[i].append((current_step[0] - 1, current_step[1]))
-                if current_step[0] + 1 < len(data) and data[current_step[0] + 1][current_step[1]] == next_elevation:
-                    next_steps[i].append((current_step[0] + 1, current_step[1]))
-                if current_step[1] - 1 >= 0 and data[current_step[0]][current_step[1] - 1] == next_elevation:
-                    next_steps[i].append((current_step[0], current_step[1] - 1))
-                if current_step[1] + 1 < len(data[0]) and data[current_step[0]][current_step[1] + 1] == next_elevation:
-                    next_steps[i].append((current_step[0], current_step[1] + 1))
-
-        current_steps = next_steps
-        next_steps = [[] for _ in range(len(trailheads))]
-        next_elevation += 1
-    head_scores = [len(trailhead) for trailhead in current_steps]
-    return head_scores
+def search_peaks(location, graph, peaks):
+    if location in peaks:
+        return [location]
+    reachable_peaks = []
+    for neighbor in graph[location]:
+        reachable_peaks += search_peaks(neighbor, graph, peaks)
+    return reachable_peaks
 
 if __name__ == "__main__":
     for file in data_files_for(os.path.basename(__file__)):
         data = [[int(i) for i in list(line.strip())] for line in file]
 
-
-        trailheads = []
-        for i, line in enumerate(data):
-            for j, num in enumerate(line):
-                if num == 0:
-                    trailheads.append((i, j))
+        reachable_peaks = find_reachable_peaks_with_multiplicity(data)
 
         print("\n--- Part one ---")
 
-
-        head_scores = calculate_head_scores(trailheads)
+        head_scores = [len(set(peaks)) for peaks in reachable_peaks]
         print(f"Total score: {sum(head_scores)}")
 
         print("\n--- Part two ---")
 
-        head_ratings = calculate_head_ratings(trailheads)
+        head_ratings = [len(peaks) for peaks in reachable_peaks]
         print(f"Total rating: {sum(head_ratings)}")
 
         # exit(0)
