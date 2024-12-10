a1f27e33f2a74c0c295439735f170859427644c9
Ivett Ördög <devillmeister@gmail.com>
2024-12-10 10:51:17 +0100

    Day10: Part 2

--- 2024/day10.py
+++ 2024/day10.py
@@ -10,12 +10,56 @@ def print_touched(data, touched):
                 print(".", end="")
         print()
 
+
+def calculate_head_scores(trailheads):
+    current_steps = [{trailhead} for trailhead in trailheads]
+    next_steps = [set() for _ in range(len(trailheads))]
+    next_elevation = 1
+    while next_elevation < 10:
+        for i, trailhead_steps in enumerate(current_steps):
+            for current_step in trailhead_steps:
+                if current_step[0] - 1 >= 0 and data[current_step[0] - 1][current_step[1]] == next_elevation:
+                    next_steps[i].add((current_step[0] - 1, current_step[1]))
+                if current_step[0] + 1 < len(data) and data[current_step[0] + 1][current_step[1]] == next_elevation:
+                    next_steps[i].add((current_step[0] + 1, current_step[1]))
+                if current_step[1] - 1 >= 0 and data[current_step[0]][current_step[1] - 1] == next_elevation:
+                    next_steps[i].add((current_step[0], current_step[1] - 1))
+                if current_step[1] + 1 < len(data[0]) and data[current_step[0]][current_step[1] + 1] == next_elevation:
+                    next_steps[i].add((current_step[0], current_step[1] + 1))
+
+        current_steps = next_steps
+        next_steps = [set() for _ in range(len(trailheads))]
+        next_elevation += 1
+    head_scores = [len(trailhead) for trailhead in current_steps]
+    return head_scores
+
+
+def calculate_head_ratings(trailheads):
+    current_steps = [[trailhead] for trailhead in trailheads]
+    next_steps = [[] for _ in range(len(trailheads))]
+    next_elevation = 1
+    while next_elevation < 10:
+        for i, trailhead_steps in enumerate(current_steps):
+            for current_step in trailhead_steps:
+                if current_step[0] - 1 >= 0 and data[current_step[0] - 1][current_step[1]] == next_elevation:
+                    next_steps[i].append((current_step[0] - 1, current_step[1]))
+                if current_step[0] + 1 < len(data) and data[current_step[0] + 1][current_step[1]] == next_elevation:
+                    next_steps[i].append((current_step[0] + 1, current_step[1]))
+                if current_step[1] - 1 >= 0 and data[current_step[0]][current_step[1] - 1] == next_elevation:
+                    next_steps[i].append((current_step[0], current_step[1] - 1))
+                if current_step[1] + 1 < len(data[0]) and data[current_step[0]][current_step[1] + 1] == next_elevation:
+                    next_steps[i].append((current_step[0], current_step[1] + 1))
+
+        current_steps = next_steps
+        next_steps = [[] for _ in range(len(trailheads))]
+        next_elevation += 1
+    head_scores = [len(trailhead) for trailhead in current_steps]
+    return head_scores
+
 if __name__ == "__main__":
     for file in data_files_for(os.path.basename(__file__)):
         data = [[int(i) for i in list(line.strip())] for line in file]
 
-
-        print("\n--- Part one ---")
 
         trailheads = []
         for i, line in enumerate(data):
@@ -23,31 +67,15 @@ if __name__ == "__main__":
                 if num == 0:
                     trailheads.append((i, j))
 
-        current_steps = [{trailhead} for trailhead in trailheads]
-        all_touched = [{trailhead} for trailhead in trailheads]
-        next_steps = [set() for _ in range(len(trailheads))]
-        next_elevation = 1
+        print("\n--- Part one ---")
 
-        while next_elevation < 10:
-            for i, trailhead_steps in enumerate(current_steps):
-                for current_step in trailhead_steps:
-                    if current_step[0] - 1 >= 0 and data[current_step[0] - 1][current_step[1]] == next_elevation:
-                        next_steps[i].add((current_step[0] - 1, current_step[1]))
-                    if current_step[0] + 1 < len(data) and data[current_step[0] + 1][current_step[1]] == next_elevation:
-                        next_steps[i].add((current_step[0] + 1, current_step[1]))
-                    if current_step[1] - 1 >= 0 and data[current_step[0]][current_step[1] - 1] == next_elevation:
-                        next_steps[i].add((current_step[0], current_step[1] - 1))
-                    if current_step[1] + 1 < len(data[0]) and data[current_step[0]][current_step[1] + 1] == next_elevation:
-                        next_steps[i].add((current_step[0], current_step[1] + 1))
-                all_touched[i] = all_touched[i].union(next_steps[i])
 
-            current_steps = next_steps
-            next_steps = [set() for _ in range(len(trailheads))]
-            next_elevation += 1
-
-        head_scores = [len(trailhead) for trailhead in current_steps]
+        head_scores = calculate_head_scores(trailheads)
         print(f"Total score: {sum(head_scores)}")
 
         print("\n--- Part two ---")
+
+        head_ratings = calculate_head_ratings(trailheads)
+        print(f"Total rating: {sum(head_ratings)}")
 
         # exit(0)
