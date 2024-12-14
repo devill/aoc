92dadd1d6173fa62c99674fd7c4275c668857a04
Ivett Ördög <devillmeister@gmail.com>
2024-12-14 12:19:25 +0100

    Day 14: part 1

--- 2024/day14.py
+++ 2024/day14.py
@@ -1,11 +1,54 @@
 import os
+import re
+import math
 from utils import data_files_for
 
+def parse_line(line):
+    match = re.match(r'p=(\d+),(\d+) v=(-?\d+),(-?\d+)', line)
+    if match:
+        p = (int(match.group(1)), int(match.group(2)))
+        v = (int(match.group(3)), int(match.group(4)))
+        return  { "position": p, "velocity": v }
+    return None
+
+def display_positions(positions, size):
+    robot_map = [[0 for _ in range(size[0])] for _ in range(size[1])]
+    for x,y in positions:
+        robot_map[y][x] += 1
+    print('\n'.join([''.join([str(c) if c > 0 else '.' for c in line]) for line in robot_map]))
+
+
 if __name__ == "__main__":
-    for file, _ in data_files_for(os.path.basename(__file__)):
-        for line in file:
-            print(line)
+    for file, type in data_files_for(os.path.basename(__file__)):
+        robots = [ parse_line(line) for line in file ]
+
+        size = (11,7) if type == "test" else (101,103)
+
 
         print("\n--- Part one ---")
 
+        initial_positions = [ r["position"] for r in robots ]
+
+
+        end_positions = [
+            ((r["position"][0] + 100 * r["velocity"][0]) % size[0],
+             (r["position"][1] + 100 * r["velocity"][1]) % size[1])
+            for r in robots
+        ]
+
+        # display_positions(end_positions, size)
+
+        quadrants = [[0,0], [0,0]]
+        half_point = ((size[0] - 1) // 2, (size[1] - 1) // 2)
+
+        for p in end_positions:
+            if p[0] != half_point[0] and p[1] != half_point[1]:
+                qx = 0 if p[0] < half_point[0] else 1
+                qy = 0 if p[1] < half_point[1] else 1
+                quadrants[qx][qy] += 1
+
+        print(math.prod(c for line in quadrants for c in line))
+
         print("\n--- Part two ---")
+
+        # exit(0)
