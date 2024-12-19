5f86eecdd0b1a1abe3abd65db3dabe6a1543a1f5
Ivett Ördög <devillmeister@gmail.com>
2024-12-19 11:57:53 +0100

    Day 19: part 2 done

--- 2024/day19.py
+++ 2024/day19.py
@@ -12,6 +12,22 @@ def select_patterns_for_design(patterns, design):
                 return [pattern] + patterns_for_rest_of_design
     return None
 
+class PatternCounter:
+    def __init__(self, patterns):
+        self.patterns = patterns
+        self.cache = {}
+
+    def count_variations_for_design(self, design):
+        if design == '':
+            return 1
+        if design in self.cache:
+            return self.cache[design]
+        count = 0
+        for pattern in self.patterns:
+            if design[:len(pattern)] == pattern:
+                count += self.count_variations_for_design(design[len(pattern):])
+        self.cache[design] = count
+        return count
 
 
 if __name__ == "__main__":
@@ -20,15 +36,19 @@ if __name__ == "__main__":
         patterns = [pattern.strip() for pattern in data[0].split(', ')]
         designs = [design.strip() for design in data[2:]]
 
+        counter = PatternCounter(patterns)
+        count_possible_designs = 0
+        count_total_variations = 0
+        for design in designs:
+            pattern_variations = counter.count_variations_for_design(design)
+            count_possible_designs += 1 if pattern_variations > 0 else 0
+            count_total_variations += pattern_variations
 
         print("\n--- Part one ---")
 
-        count = 0
-        for design in designs:
-            patterns_for_design = select_patterns_for_design(patterns, design)
-            if patterns_for_design is not None:
-                count += 1
-        print(count)
-
+        print(f"Possible designs: {count_possible_designs}")
 
         print("\n--- Part two ---")
+
+
+        print(f"Total variations: {count_total_variations}")
