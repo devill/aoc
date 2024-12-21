964bf7629391c2be63af6d1c13ea14e504c3ae44
Ivett Ördög <devillmeister@gmail.com>
2024-12-21 19:47:13 +0100

    Day 21: Refactoring

--- 2024/day21.py
+++ 2024/day21.py
@@ -1,65 +1,42 @@
 import os
 from utils import data_files_for
 
-# Define the keypads
-numeric_keypad = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    ['', '0', 'A']
-]
+class Keypad:
+    def __init__(self, layout):
+        self.layout = layout
+        self.positions = {key: (x, y) for y, row in enumerate(layout) for x, key in enumerate(row) if key}
 
-directional_keypad = [
-    ['', '^', 'A'],
-    ['<', 'v', '>']
-]
+    def shortest_paths_between_keys(self, start_key, end_key):
+        start = self.positions[start_key]
+        end = self.positions[end_key]
 
+        vertical_distance = abs(start[1] - end[1])
+        vertical_direction = '^' if start[1] > end[1] else 'v'
+        horizontal_distance = abs(start[0] - end[0])
+        horizontal_direction = '<' if start[0] > end[0] else '>'
 
-def shortest_paths_between_keys(keypad_positions, keypad_values, start_key, end_key):
-    start = keypad_positions[start_key]
-    end = keypad_positions[end_key]
+        if vertical_distance == 0 or horizontal_distance == 0:
+            yield ''.join([vertical_direction] * vertical_distance + [horizontal_direction] * horizontal_distance)
+            return
 
-    vertical_distance = abs(start[1] - end[1])
-    vertical_direction = '^' if start[1] > end[1] else 'v'
-    horizontal_distance = abs(start[0] - end[0])
-    horizontal_direction = '<' if start[0] > end[0] else '>'
+        if self.layout[end[1]][start[0]] != '':
+            yield ''.join([vertical_direction] * vertical_distance + [horizontal_direction] * horizontal_distance)
 
-    # If distance is 0 in one direction, we can only go in the other direction
-    if vertical_distance == 0 or horizontal_distance == 0:
-        yield ''.join([vertical_direction] * vertical_distance + [horizontal_direction] * horizontal_distance)
-        return
+        if self.layout[start[1]][end[0]] != '':
+            yield ''.join([horizontal_direction] * horizontal_distance + [vertical_direction] * vertical_distance)
 
-    # Unless we hit a gap
-    # we can go vertical first
-    if keypad_values[end[1]][start[0]] != '':
-        yield ''.join([vertical_direction] * vertical_distance + [horizontal_direction] * horizontal_distance)
-
-    # Unless we start on the last row and end on the first colum,
-    # we can go horizontal first
-    if keypad_values[start[1]][end[0]] != '':
-        yield ''.join([horizontal_direction] * horizontal_distance + [vertical_direction] * vertical_distance)
-
-
-def paths_for(keypad):
-    keypad_positions = { key: (x, y) for y, row in enumerate(keypad) for x, key in enumerate(row) if key }
-    keypad_paths = {
-        (start, end): [path for path in shortest_paths_between_keys(keypad_positions, keypad, start, end)]
-        for start in keypad_positions for end in keypad_positions
-    }
-    return keypad_paths
-
+    def paths_for(self):
+        return {
+            (start, end): [path for path in self.shortest_paths_between_keys(start, end)]
+            for start in self.positions for end in self.positions
+        }
 
 def add_indirection(path, min_path_length):
-    length = 0
-    total_path = 'A' + path + 'A'
-    for start, end in zip(total_path[:-1], total_path[1:]):
-        length += min_path_length[(start, end)]
-    return length
+    return sum(min_path_length[(start, end)] for start, end in zip('A' + path, path + 'A'))
 
-def calculate_complexity(data, indirections):
-    directional_keypad_paths = paths_for(directional_keypad)
-    numeric_keypad_paths = paths_for(numeric_keypad)
-
+def calculate_complexity(data, indirections, numeric_keypad, directional_keypad):
+    directional_keypad_paths = directional_keypad.paths_for()
+    numeric_keypad_paths = numeric_keypad.paths_for()
 
     min_path_length = {
         (start, end): min(len(path) for path in paths) + 1
@@ -71,7 +48,6 @@ def calculate_complexity(data, indirections):
             (start, end): min([add_indirection(path, min_path_length) for path in paths])
             for (start, end), paths in directional_keypad_paths.items()
         }
-
         min_path_length = min_indirect_path_length
 
     min_indirect_numeric_path_length = {
@@ -84,18 +60,26 @@ def calculate_complexity(data, indirections):
         min_length = sum([min_indirect_numeric_path_length[(start, end)] for start, end in zip('A' + numeric_sequence[:-1], numeric_sequence)])
         value = int(''.join(numeric_sequence[:-1]))
         complexity += min_length * value
-        # print(numeric_sequence, min_length)
     print(f"Complexity: {complexity}")
 
 if __name__ == "__main__":
-    for file, _ in data_files_for(os.path.basename(__file__)):
+    numeric_keypad = Keypad([
+        ['7', '8', '9'],
+        ['4', '5', '6'],
+        ['1', '2', '3'],
+        ['', '0', 'A']
+    ])
 
+    directional_keypad = Keypad([
+        ['', '^', 'A'],
+        ['<', 'v', '>']
+    ])
+
+    for file, _ in data_files_for(os.path.basename(__file__)):
         data = [line.strip() for line in file.readlines()]
 
         print("\n--- Part one ---")
-
-        calculate_complexity(data, 2)
+        calculate_complexity(data, 2, numeric_keypad, directional_keypad)
 
         print("\n--- Part two ---")
-
-        calculate_complexity(data, 25)
+        calculate_complexity(data, 25, numeric_keypad, directional_keypad)
