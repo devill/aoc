7441eec3df2607465e38a8b7629ef39aa9e645fd
Ivett Ördög <devillmeister@gmail.com>
2024-12-23 09:13:27 +0100

    Day 23: Part 2

--- 2024/day23.py
+++ 2024/day23.py
@@ -2,7 +2,33 @@ import os
 import re
 from itertools import product
 from utils import data_files_for
+import networkx as nx
 
+def bron_kerbosch_with_pivoting(graph, r, p, x):
+    """Bron–Kerbosch algorithm with pivoting for finding cliques."""
+    if not p and not x:
+        yield r
+    else:
+        # Choose a pivot to minimize the size of P ∩ N(pivot)
+        pivot = next(iter(p.union(x)))
+        pivot_neighbors = set(graph.neighbors(pivot))  # Convert neighbors to a set
+        for v in p - pivot_neighbors:
+            yield from bron_kerbosch_with_pivoting(
+                graph,
+                r.union({v}),
+                p.intersection(graph.neighbors(v)),
+                x.intersection(graph.neighbors(v))
+            )
+            p.remove(v)
+            x.add(v)
+
+def find_largest_clique(graph):
+    """Find the largest clique in the graph using Bron-Kerbosch with pivoting."""
+    max_clique = set()
+    for clique in bron_kerbosch_with_pivoting(graph, set(), set(graph.nodes()), set()):
+        if len(clique) > len(max_clique):
+            max_clique = clique
+    return max_clique
 
 class Graph:
     def __init__(self):
@@ -26,6 +52,13 @@ class Graph:
     def search_nodes(self, regex):
         return [(node, edges) for node, edges in self.nodes() if regex.match(node)]
 
+    def get_networkx_graph(self):
+        G = nx.Graph()
+        for node, edges in self.nodes():
+            for edge in edges:
+                G.add_edge(node, edge)
+        return G
+
 
 def parse_edge(line):
     return tuple(line.strip().split("-"))
@@ -39,7 +72,7 @@ if __name__ == "__main__":
             graph.add_edge(*edge)
 
         print("Number of nodes:", len(graph.edges))
-        print("Number of edges:", sum(len(edges) for edges in graph.edges.values()))
+        print("Number of edges:", sum(len(edges) for edges in graph.edges.values()) //2)
 
         print("\n--- Part one ---")
 
@@ -62,3 +95,8 @@ if __name__ == "__main__":
 
 
         print("\n--- Part two ---")
+
+        G = graph.get_networkx_graph()
+        largest_clique = sorted(find_largest_clique(G))
+        print("Largest Clique:", ','.join(largest_clique))
+        print("Size of Largest Clique:", len(largest_clique))
